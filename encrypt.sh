de9c46737dc3b3879a75a57ee6c7ea6587ca101b
Jason Lau <lauwtj@yahoo.com>
2015-05-10 14:05:13 +0100

    add code to handle single file - else all files

--- encrypt.sh
+++ encrypt.sh
@@ -1,18 +1,32 @@
 #!/usr/bin/bash
-
-echo "Type in password, followed by [ENTER]:"
-read password
-
-
-for filename in `ls *.avi *.mpg *.wmv *.mp4 *.mov *.mkv`;
-do
-    if [ ! -f $filename.bin ]; then
+function encrypt
+{
+	filename=$1
+	echo $filename
+	if [ ! -f $filename.bin ]; then
         echo "encrypting $filename"
         time openssl enc -aes-256-cbc -a -salt -in $filename -out $filename.bin -pass pass:$password
     else
         echo "Skipping ... $filename already encrypted"
     fi
-done
+}
+
+echo "Type in password, followed by [ENTER]:"
+read password
+
+if [ $# -eq 0 ]; then
+	for file in `ls *.avi *.mpg *.wmv *.mp4 *.mov *.mkv`;
+	do
+		encrypt $file
+	done
+else
+    if [ $# -eq 1 ]; then
+        encrypt $1 
+    else
+        echo "Usage: encrypt.sh [filename]"
+        echo "no arguments will decrypt all files"
+    fi
+fi
 
 #rm *.avi
 
