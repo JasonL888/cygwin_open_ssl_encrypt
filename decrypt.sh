c278304df9b94b44d76779184f33d929426a8c32
unknown <jason.lau@LT3341.lithium.local>
2015-05-10 09:11:18 +0100

    initial checkin to git

---
+++ decrypt.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/bash
+function decrypt 
+{
+        basefilename=`basename $1 .bin`
+        echo $basefilename
+        if [ -f $1 ]; then
+            if [ ! -f $basefilename ]; then 
+                echo "decrypting $1"
+                time openssl enc -d -aes-256-cbc -a -nopad -in $1 -out $basefilename -pass pass:$password
+            else
+                echo "already decrypted ... skipping $1"
+            fi
+        else
+            echo "no such file[$1] ... skipping $1"
+        fi
+
+}
+
+
+echo "Type in password, followed by [ENTER]:"
+read password
+
+if [ $# -eq 0 ]; then
+    echo "No arguments, decrypting all files"
+
+    for filename in `ls *.bin`;
+    do
+        decrypt $filename
+    done
+else
+    if [ $# -eq 1 ]; then
+        decrypt $1 
+    else
+        echo "Usage: decrypt.sh [filename]"
+        echo "no arguments will decrypt all files"
+    fi
+fi
+
+echo "decryption completed"
---
+++ encrypt.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+echo "Type in password, followed by [ENTER]:"
+read password
+
+
+for filename in `ls *.avi *.mpg *.wmv *.mp4 *.mov *.mkv`;
+do
+    if [ ! -f $filename.bin ]; then
+        echo "encrypting $filename"
+        time openssl enc -aes-256-cbc -a -salt -in $filename -out $filename.bin -pass pass:$password
+    else
+        echo "Skipping ... $filename already encrypted"
+    fi
+done
+
+#rm *.avi
+
+
